d0fd9746a208f8272450962439c2e5e5017310dd
dinomoon <ansrud45@gmail.com>
2020-02-19 17:51:47 +0900

    show-on-scroll 완성

--- Hankook Precision Works/show-on-scroll.js
+++ Hankook Precision Works/show-on-scroll.js
@@ -6,11 +6,24 @@ for (let i=0; i<contWrap.length; i++) {
   contWrapOST[i] = contWrap[i].getBoundingClientRect().top - offset;
 }
 
-console.log(contWrapOST);
+let section04 = document.querySelector('#section04');
+let typeOST = section04.offsetTop;
+let types = document.querySelectorAll('#section04 li');
+
 window.addEventListener('scroll', function(){
   for(let i=0; i<contWrap.length; i++) {
     if (this.scrollY > contWrapOST[i]) {
       img[i].classList.add('animate');
     }
+  }
+
+  let i = 0;
+  if (this.scrollY > typeOST - 600) {
+    setInterval(function(){
+      if (i < 6) {
+        types[i].classList.add('animate');
+        i++;
+      }
+    }, 600);
   }
 })
